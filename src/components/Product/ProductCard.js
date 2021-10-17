76c8ddc6ac3b9511df14a8de2ffc5c73332c6638
Eric Chiu <eric840906@hotmail.com.tw>
2021-10-17 16:03:05 +0800

    add transition to product card

--- src/components/Product/ProductCard.js
+++ src/components/Product/ProductCard.js
@@ -1,40 +1,39 @@
 import React from 'react'
-import { Flex, Image, Heading, Text } from '@chakra-ui/react'
+import { Flex, Image, Heading, Text, Fade } from '@chakra-ui/react'
 import PropTypes from 'prop-types'
 import { textOverflow } from '../../assets/style'
+import '../../assets/style.css'
 
 const ProductCard = ({ product }) => {
   return (
-    <Flex
-      borderRadius="10px"
-      overflow="hidden"
-      bg="brand.100"
-      flexDirection="row"
-      boxShadow="md"
-    >
-      <Image
-        boxSize={{ base: '100px', sm: '150px' }}
-        objectFit="cover"
-        src={product.imageUrl}
-      />
-      <Flex
-        overflow="hidden"
-        flexDirection="column"
-        p={3}
-        h={{ base: '100px', sm: '150px' }}
-      >
-        <Heading
-          {...textOverflow}
-          textTransform="capitalize"
-          size={{ base: '12px', md: 'md' }}
-        >
-          {product.title}
-        </Heading>
-        <Text {...textOverflow}>{product.ingredients.join(', ')}</Text>
-        <Text textAlign="end" marginTop="auto">
-          {`$${product.price}`}
-        </Text>
-      </Flex>
+    <Flex borderRadius="10px" overflow="hidden" boxShadow="md" w="100%">
+      <Fade timeout={10} in={true} style={{ display: 'flex' }}>
+        <Flex w="100%" bg="brand.100">
+          <Image
+            boxSize={{ base: '100px', sm: '150px' }}
+            objectFit="cover"
+            src={product.imageUrl}
+          />
+          <Flex
+            overflow="hidden"
+            flexDirection="column"
+            p={3}
+            h={{ base: '100px', sm: '150px' }}
+          >
+            <Heading
+              {...textOverflow}
+              textTransform="capitalize"
+              size={{ base: '12px', md: 'md' }}
+            >
+              {product.title}
+            </Heading>
+            <Text {...textOverflow}>{product.ingredients.join(', ')}</Text>
+            <Text textAlign="end" marginTop="auto">
+              {`$${product.price}`}
+            </Text>
+          </Flex>
+        </Flex>
+      </Fade>
     </Flex>
   )
 }
